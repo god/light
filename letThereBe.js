d15a39935ba93381429671a915c71684c4fe381f
Jake <jake.rozin@gmail.com>
2015-04-30 19:39:52 +0200

    I hope the G-Man can transpile.

---
+++ letThereBe.js
@@ -0,0 +1,24 @@
+// In the beginning
+function inTheBeginning() {
+
+  // God created the heaven and the earth.
+  var heaven = {}, 
+      earth;   // And the earth was without form, and void; 
+  // and darkness was upon the face of the deep. 
+  let deep = 'darkness';
+
+  // And the Spirit of God moved upon the face of the waters.
+  if (!earth) {
+    // And God said, Let there be light: and there was light.
+    let light = 'light';
+    // And God saw the light, and it was good;
+    if (light) {
+      // and God divided the light from the darkness.
+      return deep + ' / ' + light;
+    } else {
+      earth = deep;
+    }
+  }
+  return heaven + earth;
+}
+
